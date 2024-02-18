277df7e5522cc3f855fcd59b3de7ca23a5ad2e52
Chad Eymard <eymardcm@gmail.com>
2024-02-18 17:25:29 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Chad Eymard on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
