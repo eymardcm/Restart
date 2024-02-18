2f1eec4e5851bdafe2a4bda024668fbb963b6533
Chad Eymard <eymardcm@gmail.com>
2024-02-18 18:43:16 -0500

    add launch screen

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Restart
+//
+//  Created by Chad Eymard on 2/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
